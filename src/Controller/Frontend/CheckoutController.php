fdc05da327c46eb76054a3abdc991cbcd871a75d
damdam48 <damdam.formation@gmail.com>
2024-06-14 09:33:14 +0200

    fea(stripe): add payment link to stripe

--- src/Controller/Frontend/CheckoutController.php
+++ src/Controller/Frontend/CheckoutController.php
@@ -5,15 +5,19 @@ namespace App\Controller\Frontend;
 use App\Entity\User;
 use App\Entity\Address;
 use App\Form\AddressType;
+use App\Form\PaymentType;
 use App\Manager\CartManager;
+use App\Entity\Order\Payment;
+use App\Factory\StripeFactory;
 use App\Entity\Delivery\Shipping;
 use App\Form\ShippingCheckoutType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/checkout', name: 'app.checkout')]
 class CheckoutController extends AbstractController
@@ -24,6 +28,7 @@ class CheckoutController extends AbstractController
     ) {
     }
 
+    //address
     #[Route('/address', name: '.address', methods: ['GET', 'POST'])]
     public function address(Request $request): Response
     {
@@ -67,6 +72,7 @@ class CheckoutController extends AbstractController
         ]);
     }
 
+    //shipping
     #[Route('/shipping', name: '.shipping', methods: ['GET', 'POST'])]
     public function shipping(Request $request): Response
     {
@@ -105,19 +111,62 @@ class CheckoutController extends AbstractController
         ]);
     }
 
+    //recap
     #[Route('/recap', name: '.recap', methods: ['GET', 'POST'])]
-    public function recap():Response|RedirectResponse
+    public function recap(Request $request, StripeFactory $stripeFactory): Response|RedirectResponse
     {
         $cart = $this->cartManager->getCurrentCart();
 
-        if($cart->getOrderItems()->isEmpty()) {
+        if ($cart->getOrderItems()->isEmpty()) {
             $this->addFlash('danger', 'Votre panier est vide, veuillez ajouter des produits avant de continuer.');
 
             return $this->redirectToRoute('app.cart.show');
         }
 
+        $payment = (new Payment)
+            ->setStatus(Payment::STATUS_NEW)
+            ->setUser($this->getUser())
+            ->setOrderRef($cart);
+
+        $form = $this->createForm(PaymentType::class, $payment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $payment->setStatus(Payment::STATUS_NEW);
+
+            $this->em->persist($payment);
+            $this->em->flush();
+
+            $session = $stripeFactory->createSession
+            ($cart,
+            $this->generateUrl('app.checkout.success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            $this->generateUrl('app.checkout.cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        );
+
+
+            return $this->redirect($session->url);
+        }
+
+
         return $this->render('Frontend/Checkout/recap.html.twig', [
             'cart' => $cart,
+            'form' => $form,
         ]);
+    }
+
+    //success
+    #[Route('/success', name: '.success', methods: ['GET', 'POST'])]
+    public function success(): RedirectResponse
+    {
+        $this->addFlash('success', 'Vote commande a bien été enregistrée.');
+        return $this->redirectToRoute('app.home');
+    }
+
+    //cancel
+    #[Route('/cancel', name: '.cancel', methods: ['GET'])]
+    public function cancel(): RedirectResponse
+    {
+        $this->addFlash('danger', 'Vote paiment a été annulé.');
+        return $this->redirectToRoute('app.home');
     }
 }
